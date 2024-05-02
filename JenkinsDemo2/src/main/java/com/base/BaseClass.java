1ed0c3310b19f9cdbf0db86a720d5e54ea4eab62
Arfanasra <69837627+Arfanasra@users.noreply.github.com>
2024-05-02 19:17:16 +0800

    Update BaseClass.java

--- JenkinsDemo2/src/main/java/com/base/BaseClass.java
+++ JenkinsDemo2/src/main/java/com/base/BaseClass.java
@@ -1,6 +1,6 @@
 package com.base;
 
-import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.WebDriver;
 // import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
